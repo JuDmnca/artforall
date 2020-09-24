01ce14e49e4af460ab82fb50b31c0508115a12ab
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2020-09-24 15:22:38 +0200

    title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Art For All Experiment' })
+  res.render('index', { title: 'Art For All Manifesto' })
 })
 
 module.exports = router
